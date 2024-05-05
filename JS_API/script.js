e1c9affcf8de20225b873c7c94963b930e564646
Priscila Costa <costapriscilam@gmail.com>
2024-05-05 07:23:31 -0300

    JS_API andamento

---
+++ JS_API/script.js
@@ -0,0 +1,9 @@
+const containerVideos = document.querySelector(".videos__container");
+
+const api = fetch("http://localhost:3000/videos")
+.then(res => console.log())
+.then((videos) =>
+    videos.forEach((video)=> {
+        
+        })
+)
